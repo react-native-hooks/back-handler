1c4445c42b14b554939aaee5a5434b468dbad20d
pritishvaidya <pritishvaidya94@gmail.com>
2019-03-23 16:15:44 +0530

    Add sample test

---
+++ _tests_/index.test.js
@@ -0,0 +1,9 @@
+import useBackHandler from '../src';
+
+describe('Test Template function', () => {
+  it('should be a function', () => {
+    expect(
+      typeof useBackHandler,
+    ).toBe('function');
+  });
+});
