00dbf8e83efe7356e04f092b6fcaa9c19e4ac1e7
pritishvaidya <pritishvaidya94@gmail.com>
2019-03-23 16:16:22 +0530

    Add the Back Handler hook

---
+++ src/index.js
@@ -0,0 +1,26 @@
+/* eslint-disable no-unused-vars */
+import React, { useEffect, useState } from 'react';
+import { BackHandler } from 'react-native';
+
+const initialState = 0;
+
+export default (backHandler) => {
+  const [count, updateCount] = useState(initialState);
+  function handleBackPress() {
+    updateCount(prevCount => prevCount + 1);
+    if (typeof backHandler === 'function' && backHandler !== 'exit') {
+      backHandler();
+      return true;
+    }
+    return false;
+  }
+
+  useEffect(() => {
+    BackHandler.addEventListener('hardwareBackPress', handleBackPress);
+    return () => {
+      BackHandler.removeEventListener('hardwareBackPress', handleBackPress);
+    };
+  }, []);
+
+  return count;
+};
